033688b9255686f7c1b6cd20775424fdfeb02585
DakshDev <dakshkishore99@gmail.com>
2024-09-26 02:23:20 +0500

    complete loops

--- 18 Loops/index.js
+++ 18 Loops/index.js
@@ -4,14 +4,50 @@
 
 // For Loop
 
-for (let i = 1; i <= 20; i++) {
+// for (let i = 1; i <= 20; i++) {
     
-    if(i == 5){
-        console.log("Print Middle is Complete");
-        continue;
-    }
-    if(i > 10){
-        break;
-    }
-    console.log(`Print ${i} is complete`);
-}
+//     if(i == 5){
+//         console.log("Print Middle is Complete");
+//         continue;
+//     }
+//     if(i > 10){
+//         break;
+//     }
+//     console.log(`Print ${i} is complete`);
+// }
+
+
+
+
+
+
+
+
+
+
+// While Loop
+
+// let myArr = ["Flash","Hulk","Spider","Venom"];
+// let index = 1;
+// while(index <= myArr.length){
+//     console.log(`${index} Hero Name is ${myArr[index]}`);
+//     index++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// Do While Loop
+
+let index = 11;
+do{
+    console.log(`My index Value is ${index}`);
+    index += 1;
+}while (index <= 10)
